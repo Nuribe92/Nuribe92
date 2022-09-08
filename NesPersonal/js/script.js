8fd77de3c637e606a32c29d1f2cdb48884b18273
Nestor <nestoruribe92@hotmail.com>
2022-09-08 11:04:32 -0500

    1

--- NesPersonal/js/script.js
+++ NesPersonal/js/script.js
@@ -2,5 +2,5 @@ function alert1() {
     alert("Nothing else to show");
 }
 function instaLink(){
-    window.location.href = "https://www.google.com/";
+    window.location.href = "https://instagram.com/melissabhmakeup?igshid=YmMyMTA2M2Y=";
 }
