ae18dc865909d0737630be7c68890d23bf7610c6
Nestor <nestoruribe92@hotmail.com>
2022-09-10 13:09:39 -0500

    1

--- NesPersonal/js/script.js
+++ NesPersonal/js/script.js
@@ -8,5 +8,5 @@ function metaLink(){
     window.location.href = "https://instagram.com/melissabhmakeup?igshid=YmMyMTA2M2Y=";
 }
 function whatsappLink(){
-    window.location.href = "https://instagram.com/melissabhmakeup?igshid=YmMyMTA2M2Y=";
+    window.location.href = "https://api.whatsapp.com/send?phone=573157276542&text=Hola,%20Quisiera%20conocer%20mas%20de%20tus%20productos";
 }
