80322f1badf8712a0fbc398db1e0fcafccfbafef
Nestor <nestoruribe92@hotmail.com>
2022-09-10 13:15:01 -0500

    1

--- NesPersonal/js/script.js
+++ NesPersonal/js/script.js
@@ -2,11 +2,11 @@ function alert1() {
     alert("Nothing else to show");
 }
 function instaLink(){
-    window.location.href = "https://instagram.com/vanitabeautyandmakeup?igshid=YmMyMTA2M2Y=";
+    window.open("https://instagram.com/vanitabeautyandmakeup?igshid=YmMyMTA2M2Y=");
 }
 function metaLink(){
-    window.location.href = "https://instagram.com/melissabhmakeup?igshid=YmMyMTA2M2Y=";
+    window.open("https://instagram.com/melissabhmakeup?igshid=YmMyMTA2M2Y=");
 }
 function whatsappLink(){
-    window.location.href = "https://api.whatsapp.com/send?phone=573157276542&text=Hola,%20Quisiera%20conocer%20mas%20de%20tus%20productos";
+    window.open("https://api.whatsapp.com/send?phone=573157276542&text=Hola,%20Quisiera%20conocer%20mas%20de%20tus%20productos");
 }
