aeacfaa214a2c000056288c7d1eb09e9b178e6a8
Nestor <nestoruribe92@hotmail.com>
2022-09-10 12:42:12 -0500

    1

--- NesPersonal/js/script.js
+++ NesPersonal/js/script.js
@@ -2,6 +2,11 @@ function alert1() {
     alert("Nothing else to show");
 }
 function instaLink(){
+    window.location.href = "https://instagram.com/vanitabeautyandmakeup?igshid=YmMyMTA2M2Y=";
+}
+function metaLink(){
     window.location.href = "https://instagram.com/melissabhmakeup?igshid=YmMyMTA2M2Y=";
 }
-
+function whatsappLink(){
+    window.location.href = "https://instagram.com/melissabhmakeup?igshid=YmMyMTA2M2Y=";
+}
