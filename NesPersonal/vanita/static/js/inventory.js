dc82201a8b17569569c0d62747a763463d76dadd
Nuribe92 <nestoruribe92@hotmail.com>
2022-10-12 21:18:43 -0500

    added img and start shopingcar

--- NesPersonal/vanita/static/js/inventory.js
+++ NesPersonal/vanita/static/js/inventory.js
@@ -1,14 +1,65 @@
 const products = [
     {
         id: 1,
-        nombre: ,
-        precio: ,
-        img:
-
+        nombre: "Rubor y labial velvet beauty Glazed",
+        precio: 15,000,
+        img: "imgvid\rubor y labial velvet.png"
     },
+    {
+        id: 2,
+        nombre: "Soft Lips Individual",
+        precio: 6,000,
+        img: "imgvid\rubor y labial velvet.png"
+    },
+    {
+        id: 3,
+        nombre: "Contorno Cupcake",
+        precio: 15,000,
+        img: "imgvid\rubor y labial velvet.png"
+    },
+    {
+        id: 4,
+        nombre: "Espuma Desmaquillante",
+        precio: 25,000,
+        img: "imgvid\rubor y labial velvet.png"
+    },
+    {
+        id: 5,
+        nombre: "Polvo Traslucido",
+        precio: 12,000,
+        img: "imgvid\rubor y labial velvet.png"
+    },
+    {
+        id: 6,
+        nombre: "Polvo Suelto Trendy",
+        precio: 20,000,
+        img: "imgvid\rubor y labial velvet.png"
+    },
+    {
+        id: 7,
+        nombre: "Corrector + Tratamiento Trendy",
+        precio: 20,000,
+        img: "imgvid\rubor y labial velvet.png"
+    },{
+        id: 8,
+        nombre: "Lapiz De Cejas ",
+        precio: 10,000,
+        img: "imgvid\rubor y labial velvet.png"
+    },{
+        id: 9,
+        nombre: "Delineador Doble Trendy",
+        precio: 15,000,
+        img: "imgvid\rubor y labial velvet.png"
+    },{
+        id: 10,
+        nombre: "Kit de Cejas Trendy",
+        precio: 18,000,
+        img: "imgvid\rubor y labial velvet.png"
+    },
+
 ]
 let shoppingCar = [];
 
 products.forEach((item) =>){
-    
+
 }
