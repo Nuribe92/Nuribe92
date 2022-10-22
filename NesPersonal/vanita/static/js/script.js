d9eb4d403cdfb7f899e314244e4fed44b83cab9b
Nuribe92 <nestoruribe92@hotmail.com>
2022-10-22 17:39:11 -0500

    advance carrito

--- NesPersonal/vanita/static/js/script.js
+++ NesPersonal/vanita/static/js/script.js
@@ -87,7 +87,7 @@ let stockProductos = [
     }
 ]
 const contenedorProductos = document.getElementById('contenedor-productos');
-
+const contenedorCarrito = document.getElementById('carrito-contenedor')
 let carrito = []
 
 stockProductos.forEach((producto) => {
@@ -103,6 +103,7 @@ stockProductos.forEach((producto) => {
     contenedorProductos.appendChild(div)
 
     const boton = document.getElementById('agregar${producto.id}')
+
     boton.addEventListener('click', () =>{
         agregarAlCarrito(producto.id)
     })
@@ -111,6 +112,22 @@ stockProductos.forEach((producto) => {
 const agregarAlCarrito = (prodId) => {
         const item = stockProductos.find((prod) => prod.id === prodId)
         carrito.push(item)
+        actualizarCarrito()
         console.log(carrito)
 }
+
+const actualizarCarrito = ()=> {
+
+    carrito.forEach((prod) => {
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML =`
+        <p>${prod.nombre}</p>
+        <p> Precio :$ ${prod.precio}</p>
+        <p> Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick ="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
+        contenedorCarrito.appendChild(div)
+    }
+    )}
 
